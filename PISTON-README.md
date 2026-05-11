7297a7a04773d20ce79986686d543d60b328f245
classiccatlinux <classiccatlinux@gmail.com>
2026-05-11 15:30:45 -0400

    Add files via upload

---
+++ PISTON-README.md
@@ -0,0 +1,60 @@
+# PISTON
+A simple python module
+made by ClassicCatLinux
+(randomnerd41)
+
+<img width="128" height="128" alt="Your paragraph text" src="https://github.com/user-attachments/assets/06c94907-8640-400c-944c-b367b5900372" />
+
+# how to use
+first make your python project file,
+
+then put PISTON.py in the same folder 
+
+as your main file you want to use the module in.
+
+next at the very top of your main file type this:
+
+```
+from PISTON import *
+```
+***this will import: time, os, and sys***
+
+# funcs
+
+clear() - clears the screen on any OS*.
+
+wait(wait time goes here) - waits however many seconds you put.
+
+cmd(command goes here) - os.system made more simple.
+
+ms(wait time goes here) - wait however many milliseconds you put.
+
+update_debian_sudo()
+
+update_debian()
+
+debian_autoremove_sudo()
+
+update_fedora_sudo()
+
+update_fedora()
+
+osname()
+
+*only tested on linux systems!
+
+# info:
+I AM NOT AT FAULT
+FOR ANY HARM DONE
+TO ANY SYSTEMS! 
+By using PISTON
+or a program with PISTON
+in it you agree to this.
+
+made by classiccatlinux (randomnerd41).
+
+FOSS - GPL 2.0
+
+
+
+
---
+++ PISTON.py
@@ -0,0 +1,106 @@
+import time
+import os
+import sys
+import shutil
+
+# ============
+# == PISTON ==
+# == MODULE ==
+# ============
+
+# info:
+# alpha_patch3
+# FOSS - AGPL 3.0
+# classiccatlinux (randomnerd41)
+# status: waiting for beta.
+
+# I AM NOT AT FAULT
+# FOR ANY HARM DONE
+# TO ANYSYSTEMS!
+# by using PISTON
+# or a program with PISTON
+# in it you agree to this.
+
+def clear():
+    if sys.platform == "win32":
+        os.system("cls")
+    else:
+        os.system("clear")
+        
+def wait(wt):
+    time.sleep(wt)
+    
+print("PISTON-alpha_patch3-#: The PISTON module was used in this program.")
+print("PISTON-#: LINK HERE")
+wait(5)
+clear()
+
+def cmd(c):
+    os.system(c)
+
+def debian_autoremove_sudo():
+    if shutil.which("apt"):
+        os.system("sudo apt autoremove -y")
+        time.sleep(5)
+    else:
+        print("PISTON-#: Apt not found!")
+        print("If you believe this is an error, please contact the owner of this program.")
+        print("Exiting...")
+        wait(5)
+        clear()
+                       
+def update_debian_sudo():
+    if shutil.which("apt"):
+        os.system("sudo apt update -y && sudo apt full-upgrade -y")
+        time.sleep(3)
+        clear()
+    else:
+        print("PISTON-#: Apt not found!")
+        print("If you believe this is an error, please contact the owner of this program.")
+        print("Exiting...")
+        wait(5)
+        clear()
+  
+def update_debian():
+    if shutil.which("apt"):
+        os.system("apt update -y && apt full-upgrade -y")
+        time.sleep(3)
+        clear()
+    else:
+        print("PISTON-#: Apt not found!")
+        print("If you believe this is an error, please contact the owner of this program.")
+        print("Exiting...")
+        wait(5)
+        clear()
+
+def update_fedora_sudo():
+    if shutil.which("dnf"):
+        os.system("sudo dnf update -y")
+        time.sleep(3)
+        clear()
+    else:
+        print("PISTON-#: Dnf not found!")
+        print("If you believe this is an error, please contact the owner of this program.")
+        print("ending the program...")
+        wait(5)
+        clear()
+        
+def update_fedora():
+    if shutil.which("dnf"):
+        os.system("dnf update -y")
+        time.sleep(3)
+        clear()
+    else:
+        print("PISTON-#: Dnf not found!")
+        print("If you believe this is an error, please contact the owner of this program.")
+        print("ending the program...")
+        wait(5)
+        clear()
+     
+def osname():
+    name = sys.platform
+    print("OS = ", name)
+    time.sleep(3)
+    clear()
+
+ms = lambda x: time.sleep(x / 1000)
